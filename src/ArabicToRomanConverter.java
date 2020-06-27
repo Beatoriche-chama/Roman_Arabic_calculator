24fceb380b515349df99e1663480541de13abaac
Beatoriche-chama <67518913+Beatoriche-chama@users.noreply.github.com>
2020-06-27 21:57:28 +0300

    Add files via upload

---
+++ src/ArabicToRomanConverter.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.TreeMap;
+
+public class ArabicToRomanConverter {
+    private static final TreeMap<Integer, String> map = new TreeMap<>(Collections.reverseOrder());
+    static {
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+    }
+    public String intToRoman(int num) {
+        StringBuilder roman = new StringBuilder("");
+        for (Integer i: map.keySet()) {
+            for (int j = 1; j <= num / i; j++) {
+                roman.append(map.get(i));
+            }
+            num %= i;
+        }
+        return roman.toString();
+    }
+}
---
+++ src/RomanToArabicConverter.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+ public class RomanToArabicConverter {
+
+    private static final Map<Character, Integer> map = new HashMap<>(7);
+
+    static {
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+    }
+
+    public int romanToInt(String s) {
+        int sum = 0;
+        int len = s.length() - 1;
+        for (int i = 0; i < len; i++) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                sum -= map.get(s.charAt(i));
+            } else {
+                sum += map.get(s.charAt(i));
+            }
+        }
+        return sum += map.get(s.charAt(len));
+    }
+}
